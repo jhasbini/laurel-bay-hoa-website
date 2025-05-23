32d7174cdd25a438cc312ced7c47dd591f5b6a99
jhasbini <jad.hasbini@hotmail.com>
2025-05-23 15:40:19 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Laurel Bay HOA Website";
-export const SITE_DESCRIPTION = "Welcome to our website!";
+export const SITE_TITLE = "Laurel_Bay_HOA_Website";
+export const SITE_DESCRIPTION = "Welcome_to_our_website!";
